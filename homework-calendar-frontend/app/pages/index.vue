1dbb4559f3378d8b17e46efb1ae003537fcaa5cc
richmailYT <73596193+richmailYT@users.noreply.github.com>
2025-12-25 11:35:59 -0500

    feat: view assignments (very crude)

--- homework-calendar-frontend/app/pages/index.vue
+++ homework-calendar-frontend/app/pages/index.vue
@@ -1,9 +1,11 @@
 <script setup lang="ts">
 import { client } from '~/utils';
 import { useUserDataStore } from '~/stores/userDataStore';
+import type { InferResponseType } from 'hono';
 const userDataStore = useUserDataStore()
-
 const toast = useToast()
+
+const assignments = ref<InferResponseType<typeof client.assignment.$get>["data"]>([])
 
 onMounted(async () => {
     toast.add({
@@ -26,12 +28,40 @@ onMounted(async () => {
     })
     userDataStore.setLoggedIn(true)
     userDataStore.setData(json.data) //TODO: why does typescript think this can be undefined?
+
+    const assignmentsRequest = await client.assignment.$get()
+    const assignmentsJson = await assignmentsRequest.json()
+
+    if (assignmentsJson.success == false) { //TODO: why isn't hono infering the authentication middleware return type?
+        toast.add({
+            color: "error",
+            title: "Something went wrong",
+            description: assignmentsJson.data
+        })
+        return
+    }
+
+    assignments.value = assignmentsJson.data
 })
 </script>
 
 <template>
     <UContainer>
+        <UModal>
+            <UForm>
+
+            </UForm>
+        </UModal>
     </UContainer>
+    <aside>
+        <div v-for="work in assignments">
+            <h1>{{ work.title }}</h1>
+            <p>{{ work.description }}</p>
+
+            <p>Due Date: {{ new Date(work.dueDate).toLocaleDateString() }}</p>
+            <UButton>Mark as completed</UButton>
+        </div>
+    </aside>
 </template>
 
 <style lang="scss"></style>
