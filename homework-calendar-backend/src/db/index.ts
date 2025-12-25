8c70fba754d845060fab3c4736d3865d6a71d4c3
richmailYT <73596193+richmailYT@users.noreply.github.com>
2025-12-25 14:18:16 -0500

    misc(backend): add schema to db connection

--- homework-calendar-backend/src/db/index.ts
+++ homework-calendar-backend/src/db/index.ts
@@ -4,4 +4,4 @@ import { Database } from 'bun:sqlite';
 import * as schema from "./schema"
 
 const sqlite = new Database(process.env.DB_FILE_NAME!);
-export const db = drizzle({ client: sqlite });
+export const db = drizzle({ client: sqlite, schema: schema });
