3419c3b0f5d6892bc49e6a3f482c7a5ffccd01a1
richmailYT <73596193+richmailYT@users.noreply.github.com>
2025-12-27 08:45:51 -0500

    feat(fullstack): view assignments on a calendar + todo list

--- homework-calendar-backend/src/index.ts
+++ homework-calendar-backend/src/index.ts
@@ -11,7 +11,7 @@ import * as z from "zod"
 import { authentication, zValidator, type SessionVariables } from './middleware';
 
 import { db } from "./db/index"
-import { eq, lt, and, isNull } from "drizzle-orm"
+import { eq, lt, gt, and, isNull, isNotNull } from "drizzle-orm"
 
 const store = new CookieStore()
 
@@ -97,14 +97,24 @@ const app = new Hono<{ Variables: SessionVariables }>()
         } as const)
     })
 
-    .get("/assignment", authentication, async (c) => {
+    .get("/assignment/:year/:month", zValidator("query", z.object({
+        getCompletedOnly: z.string().optional()
+    })), authentication, async (c) => {
         const userData = c.get("userData")
-        const oneWeekInTheFuture = Date.now() + (7 * 24 * 60 * 60)
+        const year = parseInt(c.req.param("year"))
+        const month = parseInt(c.req.param("month"))
+
+        if (isNaN(year) || isNaN(month)) {
+            return c.json({ success: false, data: "Why is year or month NaN?" } as const, 400)
+        }
+
+        const beginningOfMonth = new Date(year, month, 1).getTime()
+        const endOfMonth = new Date(year, month + 1, 0).getTime()
         const assignments = await db.select().from(schema.assignmentsTable).where(
             and(
                 and(
-                    lt(schema.assignmentsTable.dueDate, oneWeekInTheFuture), //TODO: sort by closest due date to furthest due date, limit to 10
-                    isNull(schema.assignmentsTable.completedDate)
+                    lt(schema.assignmentsTable.dueDate, endOfMonth),
+                    gt(schema.assignmentsTable.dueDate, beginningOfMonth)
                 ),
                 eq(schema.assignmentsTable.owner, userData.id)
             )
@@ -139,6 +149,54 @@ const app = new Hono<{ Variables: SessionVariables }>()
         }).returning()
 
         return c.json({ success: true, data: assignment } as const, 201)
+    })
+
+    .patch("/assignment/:id", zValidator("json", z.object({
+        title: z.string().optional(),
+        description: z.string().optional(),
+
+        startDate: z.number().optional(),
+        dueDate: z.number().optional(),
+
+        completionDate: z.number().optional().nullable()
+    })), authentication, async (c) => {
+        const id = parseInt(c.req.param("id"))
+        const userData = c.get("userData")
+        const body = c.req.valid("json")
+        if (isNaN(id)) {
+            return c.json({ success: false, data: "Why is 'id' NaN?" } as const, 400)
+        }
+
+        const update: Partial<typeof body> = {}
+        if (body.title) {
+            update["title"] = body.title
+        }
+
+        if (body.description) {
+            update["description"] = body.description
+        }
+        if (body.startDate) {
+            update["startDate"] = body.startDate
+        }
+        if (body.dueDate) {
+            update["dueDate"] = body.dueDate
+        }
+        if (body.completionDate) {
+            update["completionDate"] = body.completionDate
+        }
+
+        if (Object.keys(update).length == 0) {
+            return c.json({ success: true, data: "Nothing changed" } as const)
+        }
+
+        const modified = await db.update(schema.assignmentsTable).set(update).where(
+            and(
+                eq(schema.assignmentsTable.id, id),
+                eq(schema.assignmentsTable.owner, userData.id)
+            )
+        )
+
+        return c.json({ success: true } as const)
     })
 
 export default {
--- homework-calendar-frontend/app/pages/index.vue
+++ homework-calendar-frontend/app/pages/index.vue
@@ -9,13 +9,13 @@ import { CalendarDate } from '@internationalized/date'
 const userDataStore = useUserDataStore()
 const toast = useToast()
 
-const assignments = ref<InferResponseType<typeof client.assignment.$get>["data"]>([])
+const assignments = ref<Extract<InferResponseType<typeof client.assignment[":year"][":month"]["$get"]>, { success: true }>["data"]>([])
 
 const today = new Date()
 const oneDayInTheFuture = new Date()
 oneDayInTheFuture.setDate(oneDayInTheFuture.getDate() + 1);
-const createAssignmentStartDate = shallowRef(new CalendarDate(today.getFullYear(), today.getMonth(), today.getDay()))
-const createAssignmentDueDate = shallowRef(new CalendarDate(oneDayInTheFuture.getFullYear(), oneDayInTheFuture.getMonth(), oneDayInTheFuture.getDay()))
+const createAssignmentStartDate = shallowRef(new CalendarDate(today.getFullYear(), today.getMonth() + 1, today.getDate()))
+const createAssignmentDueDate = shallowRef(new CalendarDate(oneDayInTheFuture.getFullYear(), oneDayInTheFuture.getMonth() + 1, oneDayInTheFuture.getDate()))
 const createAssignmentZodSchema = z.object({
     title: z.string(),
     description: z.string(),
@@ -24,6 +24,28 @@ const createAssignmentZodSchema = z.object({
 type createAssignmentSchema = z.infer<typeof createAssignmentZodSchema>
 const createAssignmentState = reactive<Partial<createAssignmentSchema>>({
     type: "assignment"
+})
+
+const viewCalendarDate = shallowRef(new CalendarDate(today.getFullYear(), today.getMonth(), today.getDay()))
+const calendarDays = computed(() => {
+    const year = viewCalendarDate.value.year
+    const month = viewCalendarDate.value.month - 1 // CalendarDate months start at 1. Date starts at 0
+
+    const firstDate = new Date(year, month, 1)
+    const lastDate = new Date(year, month + 1, 0)
+    const daysInMonth = lastDate.getDate()
+    const startingDayOfTheWeek = firstDate.getDay()
+
+    const days = []
+    for (let i = 0; i < startingDayOfTheWeek; i++) {
+        days.push(null)
+    }
+
+    for (let i = 0; i < daysInMonth; i++) {
+        days.push(i)
+    }
+
+    return days
 })
 
 onMounted(async () => {
@@ -48,13 +70,21 @@ onMounted(async () => {
     userDataStore.setLoggedIn(true)
     userDataStore.setData(json.data) //TODO: why does typescript think this can be undefined?
 
-    const assignmentsRequest = await client.assignment.$get()
+    const assignmentsRequest = await client.assignment[':year'][':month'].$get({
+        param: {
+            year: today.getFullYear().toString(),
+            month: today.getMonth().toString()
+        },
+        query: {
+            getCompletedOnly: undefined
+        }
+    })
     const assignmentsJson = await assignmentsRequest.json()
 
     if (assignmentsJson.success == false) { //TODO: why isn't hono infering the authentication middleware return type?
         toast.add({
             color: "error",
-            title: "Something went wrong",
+            title: "Something went wrong fetching pending assignments",
             description: assignmentsJson.data
         })
         return
@@ -63,8 +93,16 @@ onMounted(async () => {
     assignments.value = assignmentsJson.data
 })
 
+function getEventsForDay(day: number) {
+    return assignments.value.filter(d => {
+        const dueDate = new Date(d.dueDate)
+        if (dueDate.getDate() == day) {
+            return true
+        }
+    })
+}
+
 async function onSubmitCreateAssignment(event: FormSubmitEvent<createAssignmentSchema>) {
-    toast.add({ title: "got it " })
     const create = await client.assignment.$post({
         json: {
             title: event.data.title,
@@ -94,48 +132,191 @@ async function onSubmitCreateAssignment(event: FormSubmitEvent<createAssignmentS
     assignments.value.push(response.data) //TODO: why is this infered as type | undefined?
     return
 }
+
+async function toggleAssignmentAsCompleted(id: number) {
+    const item = assignments.value.find(work => work.id == id)
+    if (item == undefined) {
+        return
+    }
+
+    const value = item.completionDate ? null : Date.now()
+    const request = await client.assignment[':id'].$patch({
+        param: {
+            id: id.toString()
+        },
+        json: {
+            completionDate: value
+        }
+    })
+    const response = await request.json()
+
+    if (response.success == false) {
+        toast.add({
+            color: "error",
+            title: "Something went wrong",
+            description: response.data
+        })
+        return
+    }
+
+    let index;
+    assignments.value.forEach((item, i) => {
+        if (item.id == id) {
+            assignments.value[i]!.completionDate = value
+        }
+        index = i
+    })
+    toast.add({
+        color: "success",
+        title: `${assignments.value[index!].title} marked as ${value ? 'completed' : "uncompleted"}!`
+    })
+}
 </script>
 
 <template>
-    <UContainer>
-        <UModal>
-            <UButton>Create assignment</UButton>
+    <main>
+        <div>
+            <header>
+                <h1>Calendar - Showing assigments for {{
+                    ["January", "February", "March", "April", "May", "June", "July",
+                        "August", "September", "October", "November", "December"].at(viewCalendarDate.month) }}
+                </h1>
 
-            <template #content>
-                <UForm :schema="createAssignmentZodSchema" :state="createAssignmentState"
-                    @submit="onSubmitCreateAssignment">
+                <UModal>
+                    <UButton>Create assignment</UButton>
 
-                    <UFormField label="Title">
-                        <UInput placeholder="World History assignment" v-model="createAssignmentState.title" />
-                    </UFormField>
-                    <UFormField label="Description">
-                        <UTextarea placeholder="For Mr. Smiths class" v-model="createAssignmentState.description" />
-                    </UFormField>
+                    <template #content>
+                        <UForm :schema="createAssignmentZodSchema" :state="createAssignmentState"
+                            @submit="onSubmitCreateAssignment">
 
-                    <UFormField label="Start Date">
-                        <UInputDate v-model="createAssignmentStartDate" />
-                    </UFormField>
+                            <UFormField label="Title">
+                                <UInput placeholder="World History assignment" v-model="createAssignmentState.title" />
+                            </UFormField>
+                            <UFormField label="Description">
+                                <UTextarea placeholder="For Mr. Smiths class"
+                                    v-model="createAssignmentState.description" />
+                            </UFormField>
 
-                    <UFormField label="Due Date">
-                        <UInputDate v-model="createAssignmentDueDate" />
-                    </UFormField>
+                            <UFormField label="Start Date">
+                                <UInputDate v-model="createAssignmentStartDate" />
+                            </UFormField>
 
-                    <UButton type="submit">
-                        Submit
-                    </UButton>
-                </UForm>
-            </template>
-        </UModal>
-    </UContainer>
-    <aside>
-        <UCard v-for="work in assignments">
-            <h1>{{ work.title }}</h1>
-            <p>{{ work.description }}</p>
+                            <UFormField label="Due Date">
+                                <UInputDate v-model="createAssignmentDueDate" />
+                            </UFormField>
 
-            <p>Due Date: {{ new Date(work.dueDate).toLocaleDateString() }}</p>
-            <UButton>Mark as completed</UButton>
-        </UCard>
-    </aside>
+                            <UButton loading-auto type="submit">
+                                Submit
+                            </UButton>
+                        </UForm>
+                    </template>
+                </UModal>
+            </header>
+
+            <div class="calendar">
+                <div class="weekday-headers">
+                    <div v-for="day in ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat']" :key="day">
+                        {{ day }}
+                    </div>
+                </div>
+
+                <div class="days-container">
+                    <div class="day" v-for="day in calendarDays"
+                        :class="{ 'has-assignments': getEventsForDay(day).length > 0 }">
+                        {{ day }}
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <aside>
+            <UTabs :items="[
+                {
+                    label: 'Uncompleted',
+                    slot: 'uncompleted'
+                },
+                {
+                    label: 'Completed',
+                    slot: 'completed'
+                }
+            ]">
+                <template #uncompleted>
+                    <UCard v-for="work in assignments" :key="work.id" v-show="work.completionDate == null">
+                        <h1>{{ work.title }}</h1>
+                        <p class="desc">{{ work.description }}</p>
+
+                        <p>Due Date: {{ new Date(work.dueDate).toLocaleDateString() }}</p>
+                        <UButton loading-auto @click="toggleAssignmentAsCompleted(work.id)">{{
+                            work.completionDate ? "Unmark as completed" : "Mark as completed" }}</UButton>
+                    </UCard>
+                </template>
+
+                <template #completed>
+                    <UCard v-for="work in assignments" :key="work.id" v-show="work.completionDate !== null">
+                        <h1>{{ work.title }}</h1>
+                        <p class="desc">{{ work.description }}</p>
+
+                        <p>Due Date: {{ new Date(work.dueDate).toLocaleDateString() }}</p>
+                        <UButton loading-auto @click="toggleAssignmentAsCompleted(work.id)">{{
+                            work.completionDate ? "Unmark as completed" : "Mark as completed" }}</UButton>
+                    </UCard>
+                </template>
+            </UTabs>
+        </aside>
+    </main>
 </template>
 
-<style lang="scss"></style>
+<style lang="scss" scoped>
+main {
+    display: flex;
+    flex-direction: row;
+    justify-content: space-around;
+    padding-top: 2rem;
+
+    header {
+        h1 {
+            font-weight: bold;
+            font-size: 48px;
+        }
+    }
+
+    .calendar {
+        .weekday-headers {
+            display: grid;
+            grid-template-columns: repeat(7, 1fr);
+            gap: 1px;
+        }
+
+        .days-container {
+            display: grid;
+            grid-template-columns: repeat(7, 1fr);
+            gap: 1px;
+
+            .day {
+                display: border-box;
+
+                padding: 0.5rem;
+                min-height: 120px;
+                max-width: 100%;
+
+                &.has-assignments {
+                    border: 1px solid green;
+                }
+            }
+        }
+    }
+
+    aside {
+        display: flex;
+        flex-direction: column;
+        gap: 1rem;
+
+        width: 16rem;
+
+        .desc {
+            color: grey;
+            font-size: 16px;
+        }
+    }
+}
+</style>
