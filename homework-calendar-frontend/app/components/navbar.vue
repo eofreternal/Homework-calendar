0b533a721f6c08e9e79899242a841d55b084ef0e
richmailYT <73596193+richmailYT@users.noreply.github.com>
2026-01-30 15:09:33 -0500

    feat(frontend): navbar

---
+++ homework-calendar-frontend/app/components/navbar.vue
@@ -0,0 +1,25 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+    <nav>
+        <NuxtLink to="/">Calendar</NuxtLink>
+        <NuxtLink to="/classes">Classes</NuxtLink>
+    </nav>
+</template>
+
+<style lang="scss" scoped>
+nav {
+    display: flex;
+    flex-direction: row;
+    gap: 2rem;
+
+    padding: 1.5rem;
+
+    a {
+        font-size: 1.75rem;
+        font-weight: 600;
+    }
+}
+</style>
