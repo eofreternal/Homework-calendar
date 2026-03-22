532b206dac6465acb2eb6fb2ec78f024697492d5
richmailYT <73596193+richmailYT@users.noreply.github.com>
2026-03-22 09:38:40 -0400

    feat(backend): automatically create/migrate db on startup

--- homework-calendar-backend/src/db/index.ts
+++ homework-calendar-backend/src/db/index.ts
@@ -1,9 +1,13 @@
 import { drizzle } from 'drizzle-orm/bun-sqlite';
 import { Database } from 'bun:sqlite';
 import * as schema from "./schema"
+import { migrate } from 'drizzle-orm/bun-sqlite/migrator';
 
 import { parseConfig } from "../utils"
 const config = parseConfig()
 
 const sqlite = new Database(config.DB_FILE_NAME);
-export const db = drizzle({ client: sqlite, schema: schema });
+const db = drizzle({ client: sqlite, schema: schema });
+migrate(db, { migrationsFolder: './drizzle' });
+
+export { db };
